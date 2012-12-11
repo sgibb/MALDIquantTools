35701f71d935350301d59ac421aa30b3b8b9221c
Sebastian Gibb <mail@sebastiangibb.de>
2012-12-11 20:05:04 +0100

    remove all import/export functions

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -19,12 +19,6 @@ if (is.null(getGeneric(".composeFilename")))
   setGeneric(".composeFilename", function(x, ...) standardGeneric(".composeFilename"))
 if (is.null(getGeneric("detectMonoisotopicPeaks")))
   setGeneric("detectMonoisotopicPeaks", function(object, ...) standardGeneric("detectMonoisotopicPeaks"))
-if (is.null(getGeneric("export")))
-  setGeneric("export", function(x, ...) standardGeneric("export"))
-if (is.null(getGeneric("exportCsv")))
-  setGeneric("exportCsv", function(x, ...) standardGeneric("exportCsv"))
-if (is.null(getGeneric("exportMsd")))
-  setGeneric("exportMsd", function(x, ...) standardGeneric("exportMsd"))
 if (is.null(getGeneric("fwhm")))
   setGeneric("fwhm", function(spec, peaks) standardGeneric("fwhm"))
 if (is.null(getGeneric(".fwhm")))
@@ -33,35 +27,4 @@ if (is.null(getGeneric(".isotopicScore")))
   setGeneric(".isotopicScore", function(object, ...) standardGeneric(".isotopicScore"))
 if (is.null(getGeneric("monoisotopic")))
   setGeneric("monoisotopic", function(object, ...) standardGeneric("monoisotopic"))
-if (is.null(getGeneric("write.csv")))
-  setGeneric("write.csv", function(...) standardGeneric("write.csv"))
-
-## make a S3 generic for write.table
-#write.table <- function(x, ...) UseMethod("write.table")
-
-## take the usual definition of write.table and set it to be the default method
-#write.table.default <- utils::write.table
-#setMethod(f="write.table", signature=signature(x="ANY"), function(x, ...)
-#          UseMethod("write.table"))
-#setMethod(f="write.table",
-#  signature=signature(x="ANY", file="character",
-#                      append="logical", quote="logical", sep="character",
-#                      eol="character", na="character", dec="character",
-#                      row.names="ANY", col.names="ANY",
-#                      qmethod="character", fileEncoding="character"),
-#  definition=function(x, file="", append=FALSE, quote=TRUE, sep=" ", eol="\n",
-#                      na="NA", dec=".", row.names=TRUE, col.names=TRUE, 
-#                      qmethod=c("escape", "double"), fileEncoding="") {
-#          UseMethod("write.table")})
-
-## allow ... argument
-#formals(write.table.default) <- c(formals(write.table.default), alist(...=))
-
-#if (is.null(getGeneric("write.table")))
-#  setGeneric("write.table", function(x, file, append, quote, sep, eol, na, dec,
-#                                     row.names, col.names, qmethod,
-#                                     fileEncoding)
-#                                     standardGeneric("write.table"))
-if (is.null(getGeneric("write.table")))
-  setGeneric("write.table")
 
--- inst/tests/test_cleanFilename.R
+++
@@ -1,7 +0,0 @@
-context("filename-functions")
-
-test_that(".cleanFilename", {
-  expect_equal(MALDIquantTools:::.cleanFilename(
-                "/home/a:/\"foo&bar\"/g.\\23!/ foo-bar?.txt"),
-               "_home_a_foo_bar_g_23_foo_bar_txt")
-})
